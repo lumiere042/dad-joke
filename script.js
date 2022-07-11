eafd2ed2fe594212e43350e1ae18e8c04549d892
lumiere <melbourne14gabriel@gmail.com>
2022-07-11 20:48:03 +0800

    done api call

--- script.js
+++ script.js
@@ -3,8 +3,17 @@ const btn = document.querySelector('.btn')
 btn.addEventListener('click', getJoke())
 
 async function getJoke(){
-    const res = await fetch('https://icanhazdadjoke.com');
-    const data = await res.json()
-
-    console.log(data);
+    // Input headers using the API
+    const response = await fetch('https://icanhazdadjoke.com',{
+        headers: {
+            Accept: 'application/json',
+            'User-Agent': 'learning app',
+        },
+    });
+    // Turn response to json format
+    const data = await response.json();
+    console.log(data['joke']);
 }
+
+
+
